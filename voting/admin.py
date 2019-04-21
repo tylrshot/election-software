6a259eef7b914bc65674a4336552683fe75e832c
tylrshot <taschott@gmail.com>
2019-04-21 12:23:24 -0400

    Add

--- voting/admin.py
+++ voting/admin.py
@@ -4,3 +4,7 @@ from django.contrib import admin
 
 from voting.models import Poll, Question, Group, Student, UserVotes
 
+admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Student)
+admin.site.register(UserVotes)
